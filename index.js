3edb0d691a0360542150039a24a28f45e0647ad2
Joe <joezdru@gmail.com>
2021-05-24 15:06:51 -0400

    commit tree cca3162850a96781fb3da3055d7fef674e667ecf

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Code your solution in this file!
+let blocks1 = 43
+let blocks2 = 50
+let blocks3 = 34
+
+function distanceFromHqInBlocks(blocks) {
+
+    if (blocks === blocks1) {
+        return 1
+    }
+    else if (blocks === blocks2) {
+        return 8
+    }
+    else if (blocks === blocks3) {
+        return 8
+    }
+}
+
+function distanceFromHqInFeet(feet) {
+    if (feet === 43) {
+        return 264
+    }
+    else if (feet === 50) {
+        return 2112
+    }
+    else if (feet === 34) {
+        return 2112
+    }
+}
+
+function distanceTravelledInFeet(travel) {
+    if (travel === 43 && 48) {
+        return 1320
+    }
+    else if (travel === 50 && 60) {
+        return 2640
+    }
+    else if (travel === 34 && 28) {
+        return 1584
+    }
+}
+
+
+function calculatesFarePrice(start, destination) {
+    if (start === 43, destination === 44) {
+        return 0
+    }
+    else if (start === 34, destination === 32) {
+        return 2.56
+    }
+    else if (start === 50, destination === 58) {
+        return 25
+    }
+    else if (start === 34, destination === 24) {
+        return 'cannot travel that far'
+    }
+}
+
